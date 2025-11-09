3288727f153a798e7f14e8301561b96b3fa14cec
gisketch <dev@gisketch.com>
2025-11-09 15:02:31 +0800

    add powershell stuff

---
+++ powershell-profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+Invoke-Expression (& { (zoxide init powershell | Out-String) }) 2>$null | Out-Null
+oh-my-posh init pwsh --config ~/.config/oh-my-posh/theme.omp.json 2>$null | Invoke-Expression
+cls
+# pokemonshow
